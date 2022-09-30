df996743e81d62ab3ec220679714ae19933f3aca
KomiKage <bryanvd654@gmail.com>
2022-09-30 14:04:42 +0200

    added chatlogging

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,9 +1,20 @@
+const fs = require('fs');
+
 module.exports={
     name: 'messageCreate',
 	execute(message){
 
-        console.log(`${message.member.displayName} : ${message} | ${message.channel.name} | ${message.createdAt}`);
+        var content = `${message.member.displayName} : ${message} | ${message.channel.name} | ${message.createdAt}`;
+
+        console.log(content);
         
+        fs.appendFile(`./chatLogs/${dateSTR}.txt`, content + '\n', err => {
+            if (err) {
+              console.error(err);
+            }
+            // file written successfully
+          });
+
         /*if(message.author != 973553585210142721){
             message.reply('awooooo')
         
--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,17 @@
 const { ActivityType } = require('discord.js');
+var fs = require("fs");
+var date = new Date();
 
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
         client.user.setActivity('a shit eating contest', { type: ActivityType.Competing });
+		date.getDate();
+		global.dateSTR = date.toDateString();
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+		if(fs.existsSync(`chatLogs/${dateSTR}.txt`)){
+			return;
+		} else{var createStream = fs.createWriteStream(`chatLogs/${dateSTR}.txt`); createStream.end();}
+	}
 };
