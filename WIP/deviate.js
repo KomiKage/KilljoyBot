e748a55b58300f7e235b4b68c8e4cafb1e28dc4b
KomiKage <bryanvd654@gmail.com>
2022-09-30 12:46:02 +0200

    created deviate.js, moved unfinished commands to wip

---
+++ WIP/deviate.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios').default;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('deviate')
+		.setDescription('Currently doesn\'t Work.')
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('the search query')
+                .setRequired(true)),
+	async execute(interaction) {
+                    let response = await axios.get(`https://backend.deviantart.com/rss.xml?type=deviation&q=boost%3Apopular+in%3Adigitalart%2Fdrawings+${"input"}`);
+                    this.image = response.data[0]; // the response is an Array, so just use the first item as the Image
+
+        await interaction.reply(this);
+	},
+};
+
